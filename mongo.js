ab625a1f9e161e19414b44533a6b105091ff7560
MarvCodeReady <MarvCodeReady@gmail.com>
2024-08-15 16:13:23 -0700

    Added ESlint to the web apllication to fix warnings

--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length<3){
-    console.log('give password as argument');
-    process.exit(1)
-    
+  console.log('give password as argument')
+  process.exit(1)
+
 }
 
 const password = process.argv[2]
@@ -17,9 +17,9 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    
+  name: String,
+  number: String,
+
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -27,27 +27,27 @@ const Person = mongoose.model('Person', personSchema)
 
 if(name && number){
 
-    const person = new Person({
-        name,
-        number,
-    })
+  const person = new Person({
+    name,
+    number,
+  })
 
-    person.save().then((result) => {
-        console.log(`Added ${name} number ${number} to phonebook`)
-        mongoose.connection.close();
-    }).catch(err => {
-        console.error('Error saving the person', err)
-        mongoose.connection.close();
-    })
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  }).catch(err => {
+    console.error('Error saving the person', err)
+    mongoose.connection.close()
+  })
 } else {
-    Person.find({}).then((result) => {
-        console.log('phonebook:')
-        result.forEach((person) => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close();
-    }).catch(err => {
-        console.error('Error Fetching the person data', err)
-        mongoose.connection.close();
+  Person.find({}).then((result) => {
+    console.log('phonebook:')
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  }).catch(err => {
+    console.error('Error Fetching the person data', err)
+    mongoose.connection.close()
+  })
 }
